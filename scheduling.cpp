ac10d273468293c7bd30dd7bf74fad5247c7bfe7
kayobera13 <65854056+kayobera13@users.noreply.github.com>
2022-01-09 00:19:11 +0200

    First c++ program updates

--- scheduling.cpp
+++ scheduling.cpp
@@ -3,13 +3,28 @@
 #include<algorithm>
 using namespace std;
 
-void executionProcess(int *eProcesses, int eProcessTE, int etime){
+struct processStructure {
+    // Array within the structure
+    int processus[6];
+};
+
+void executionProcess(processStructure *eProcesses, int eProcessTE, int etime){
 	int counter=0; 
-	for(int i=0; i<sizeof(eProcesses); i++){
-		int *process = &eProcesses[i];
-		//if(process[5]==0 && process[1]>=etime){
-		//	process[5]=1;
-		//}else{
+	cout<<"executionProcess start\n";
+	for(int i=0; i<5; i++){
+		int *process = eProcesses[i].processus;
+		if (i==eProcessTE && process[4]==0){
+			process[0]=process[0]-1;
+			if(process[0]==0){
+					process[4]==1;
+				}
+		}else{
+			process[3]=process[3]+1;
+		}
+		
+		/*if(process[5]==0 && process[1]>=etime){
+			process[5]=1;
+		}else{
 			if(counter==eProcessTE){
 				process[0]=process[0]-1;
 				if(process[0]==0){
@@ -18,20 +33,23 @@ void executionProcess(int *eProcesses, int eProcessTE, int etime){
 			}else{
 				process[3]=process[3]+1;
 			}
-		//}
+		}*/
 		counter++;
 	}
 }
 
-int schedulingProcessChoice(int *scProcesses, int type){
+int schedulingProcessChoice(processStructure *scProcesses, int type){
 	int processSelected =-1;
 	int lowestProcess = 0;
 	int counter=0;
 	bool boolProcessSelected =false;
-	for(int i=0; i<sizeof(scProcesses); i++){
-		int *process = &scProcesses[i];
-		if((boolProcessSelected=false && process[5]==1) || 
-			(boolProcessSelected=true && process[5]==1 && process[4]==0 && process[type]<lowestProcess))
+	cout<<"schedulingProcessChoice start\n";
+	//cout<<sizeof(scProcesses)<<"\n";
+	for(int i=0; i<5; i++){
+	//	cout<<scProcesses[i].processus[0]<<"\n";
+		int *process = scProcesses[i].processus;
+		if((boolProcessSelected==false && process[5]==0) || 
+			(boolProcessSelected==true && process[4]==0 && process[type]<lowestProcess))
 		{
 				lowestProcess=process[type];
 				processSelected=counter;
@@ -39,12 +57,14 @@ int schedulingProcessChoice(int *scProcesses, int type){
 		}
 		counter++;
 	}
+	return processSelected;
 }
 
 
-void schedulingPreemptive(int *pProcesses, int type){
+void schedulingPreemptive(processStructure *pProcesses, int type){
 	bool tester=true;
 	int time=0;
+	cout<<"schedulingPreemptive start\n";
 	while(tester==true){
 		int processesToExecute=schedulingProcessChoice(pProcesses, type);
 		if(processesToExecute==-1){
@@ -55,19 +75,29 @@ void schedulingPreemptive(int *pProcesses, int type){
 		}
 		time++;
 	}
+	cout<<"Scheduling Finish\n";
+	cout<<time<<"time\n";
 }
-
+/*
 int p1[6]={5, 0, 3, 0, 0, 0};
 int p2[6]={4, 1, 2, 0, 0, 0};
 int p3[6]={3, 1, 1, 0, 0, 0};
 int p4[6]={4, 2, 2, 0, 0, 0};
 int p5[6]={3, 3, 1, 0, 0, 0};
-int processes[5][6]={
-	*p1,
-	*p2,
-	*p3,
-	*p4,
-	*p5
+*/
+
+struct processStructure P1 = { {5, 0, 3, 0, 0, 0} };
+struct processStructure P2 = { {4, 1, 2, 0, 0, 0}};
+struct processStructure P3 = {{3, 1, 1, 0, 0, 0} };
+struct processStructure P4 = { {4, 2, 2, 0, 0, 0} };
+struct processStructure P5 = { {3, 3, 1, 0, 0, 0} };
+
+processStructure processes[5]={
+	P1,
+	P2,
+	P3,
+	P4,
+	P5
 };
   
   int main ()
@@ -100,6 +130,7 @@ int processes[5][6]={
 					case 2:
 						cout<<"First Come, First Served Scheduling\n";
 						schedulingPreemptive(processes, 1);
+						
 						break;
 					case 3:
 						cout<<"Shortest-Job-First Scheduling\n";
